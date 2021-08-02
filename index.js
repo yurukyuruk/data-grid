5baa4ef48171f186ad423efecba1f5f399d1fdfc
yurukyuruk <yurukyuruk@gmail.com>
2021-08-02 20:31:35 +0300

    address section works

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const addressSection = document.querySelector("#address-section");
-const allAddressSectionElements = document.querySelectorAll("[data-address-section-expanded]");
-const addressSummaryElement = document.querySelector("[data-address-section-closed]");
-
-addressSection.addEventListener("click", () => {
-    const addressSectionElementsStateChange = addressSection.getAttribute("data-address-section-expanded") === "false" ? true : false;
-    const addressSummaryElementState = !addressSectionElementsStateChange;
-    allAddressSectionElements.forEach(addressElement => addressElement.setAttribute("data-address-section-expanded", addressSectionElementsStateChange.toString()));
-    addressSummaryElement.setAttribute("data-address-section-closed", addressSummaryElementState.toString());
-    
-})
-
+let allAddressSectionElements;
+let addressSummaryElements;
 window.addEventListener("load", getApi);
 function getApi() {
     fetch("https://raw.githubusercontent.com/kanow-blog/kanow-school-javascript-basics/master/projects/project-2/personData/persons-data-0.json").then(response => response.json()).then(users => {
@@ -22,31 +13,34 @@ function getApi() {
             let newClassName = changeClassName(n);
             newDataRow.className = newClassName;
             dataRows.appendChild(newDataRow);
-            
-            newDataRow.querySelector(combineStrings(newClassName, ".id-data")).innerHTML = user.id;
-            newDataRow.querySelector(combineStrings(newClassName, ".gender-data")).innerHTML = user.gender;
-            newDataRow.querySelector(combineStrings(newClassName, ".first-name-data")).innerHTML = user.firstName;
-            newDataRow.querySelector(combineStrings(newClassName, ".last-name-data")).innerHTML = user.lastName;
-            newDataRow.querySelector(combineStrings(newClassName, ".birthday-data")).innerHTML = user.birthDate;
-            newDataRow.querySelector(combineStrings(newClassName, ".age-data")).innerHTML = `${user.age}`;
-            newDataRow.querySelector(combineStrings(newClassName, ".email-data")).innerHTML = user.email;
-            newDataRow.querySelector(combineStrings(newClassName, ".address-summary-data")).innerHTML = `${user.address.city} ${user.address.street} ${user.address.houseNumber}`;
-            newDataRow.querySelector(combineStrings(newClassName, ".country-data")).innerHTML = user.address.country;
-            newDataRow.querySelector(combineStrings(newClassName, ".state-data")).innerHTML = user.address.state;
-            newDataRow.querySelector(combineStrings(newClassName, ".city-data")).innerHTML = user.address.city;
-            newDataRow.querySelector(combineStrings(newClassName, ".street-data")).innerHTML = user.address.street;
-            newDataRow.querySelector(combineStrings(newClassName, ".house-number-data")).innerHTML = `${user.address.houseNumber}`;
-
+            newDataRow.querySelector(`.${newClassName} .id-data`).innerHTML = user.id;
+            newDataRow.querySelector(`.${newClassName} .gender-data`).innerHTML = user.gender;
+            newDataRow.querySelector(`.${newClassName} .first-name-data`).innerHTML = user.firstName;
+            newDataRow.querySelector(`.${newClassName} .last-name-data`).innerHTML = user.lastName;
+            newDataRow.querySelector(`.${newClassName} .birthday-data`).innerHTML = user.birthDate;
+            newDataRow.querySelector(`.${newClassName} .age-data`).innerHTML = `${user.age}`;
+            newDataRow.querySelector(`.${newClassName} .email-data`).innerHTML = user.email;
+            newDataRow.querySelector(`.${newClassName} .address-summary-data`).innerHTML = `${user.address.city} ${user.address.street} ${user.address.houseNumber}`;
+            newDataRow.querySelector(`.${newClassName} .country-data`).innerHTML = user.address.country;
+            newDataRow.querySelector(`.${newClassName} .state-data`).innerHTML = user.address.state;
+            newDataRow.querySelector(`.${newClassName} .city-data`).innerHTML = user.address.city;
+            newDataRow.querySelector(`.${newClassName} .street-data`).innerHTML = user.address.street;
+            newDataRow.querySelector(`.${newClassName} .house-number-data`).innerHTML = `${user.address.houseNumber}`;
             if(users.indexOf(user) % 2 !== 0) {
                 newDataRow.style.backgroundColor = "rgb(253, 206, 184)";
             }
-
         })
+        allAddressSectionElements = document.querySelectorAll("[data-address-section-expanded]");
+        addressSummaryElements = document.querySelectorAll("[data-address-section-closed]");
     })
 }
 function changeClassName(newNumber) {
     return `row${newNumber}`;
 }
-function combineStrings(string1, string2) {
-    return `.${string1} ${string2}`; 
-}
+addressSection.addEventListener("click", () => {
+    const addressSectionElementsStateChange = addressSection.getAttribute("data-address-section-expanded") === "false" ? true : false;
+    const addressSummaryElementState = !addressSectionElementsStateChange;
+    allAddressSectionElements.forEach(addressElement => addressElement.setAttribute("data-address-section-expanded", addressSectionElementsStateChange.toString()));
+    addressSummaryElements.forEach(element => element.setAttribute("data-address-section-closed", addressSummaryElementState.toString()));
+
+})
