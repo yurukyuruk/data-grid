027063782f389c7eff686704f5fb5381bba9ca79
yurukyuruk <yurukyuruk@gmail.com>
2021-08-17 23:15:24 +0300

    changes on css

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let addressSummaryElements;
 function createReferenceElement() {
     const dataRow = document.createElement("tr");
     dataRow.classList.add("row0");
+    dataRow.classList.add("data-row");
     const idData = document.createElement("td");
     idData.classList.add("id-data");
     const genderData = document.createElement("td");
@@ -57,11 +58,21 @@ function addDataToElement(element, eachPerson) {
     element.children[12].textContent = eachPerson.address.houseNumber;
 }
 
+
+function addClassName(newNumber) {
+    return `row${newNumber} data-row`;
+}
+
+let n = -1;
+let allPersonsElements;
 function addAllDataAtOnce(fetchedData, dataReferenceElement) {
-    const allPersonsElements = document.createDocumentFragment();
+    allPersonsElements = document.createDocumentFragment();
     fetchedData.forEach(person =>{
-        const personElement = dataReferenceElement.cloneNode(true);
+        n += 1;
+        let personElement = dataReferenceElement.cloneNode(true);
+        
         addDataToElement(personElement, person);
+        personElement.className = addClassName(n);
         allPersonsElements.append(personElement);
         if(fetchedData.indexOf(person) % 2 !== 0) {
             personElement.classList.add("colored-row");
@@ -73,9 +84,9 @@ function addAllDataAtOnce(fetchedData, dataReferenceElement) {
     addressSummaryElements = document.querySelectorAll("[data-address-section-closed]");
 }
 
-
+let data;
 fetch("https://raw.githubusercontent.com/kanow-blog/kanow-school-javascript-basics/master/projects/project-2/personData/persons-data-0.json").then(async (response) => {
-    const data = await response.json();
+    data = await response.json();
     addAllDataAtOnce(data, createReferenceElement());
 });
 
@@ -88,22 +99,56 @@ addressSection.addEventListener("click", () => {
 })
 
 const sortAddingButton = document.querySelector(".sort-adding-button");
+const columnCount = document.querySelector(".columns").childElementCount;
+const sortOptions1 = document.querySelector(".sort-options-1");
+let clickCount = 0;
+let sortOptionsList = [sortOptions1];
 sortAddingButton.addEventListener("click", () => {
-    const submitButtons = document.querySelector(".submit-buttons");
+    clickCount += 1;
     const sorting = document.querySelector(".sorting");
-    const sortOptions = document.querySelector(".sort-options");
-    const sortOptionsCloned = sortOptions.cloneNode(true);
-    sortOptionsCloned.classList = "sort-options-cloned";
-    sorting.insertBefore(sortOptionsCloned, submitButtons);
-    const additionSymbol = document.createElement("p");
-    additionSymbol.innerText = "+";
-    additionSymbol.classList = "addition-symbol";
-    sorting.insertBefore(additionSymbol, sortOptionsCloned);
+    let sortOptionsCloned = sortOptions1.cloneNode(true);
+    sortOptionsCloned.classList = `sort-options-${clickCount + 1}`;
+    sorting.insertBefore(sortOptionsCloned, sortAddingButton);
+    sortOptionsList.push(sortOptionsCloned);
+    if(clickCount > columnCount - 2) {
+        sortAddingButton.disabled = true;
+    }
 })
+
+
+const selectFields = document.querySelector(".sort-options-1 .sort-fields");
+let currentSelectedOptions = [];
+selectFields.addEventListener("change", function() {
+    let selectedOption = selectFields.options[selectFields.selectedIndex].value;
+    currentSelectedOptions.push(selectedOption);
+    if(currentSelectedOptions.includes(selectedOption) === false && kaldırılmış) {
+        //yeniden ekle /seçilip kaldırılmış mı kalkmışsa yeniden eklenecek.
+    }
+    
+    if(selectedOption === "id") {
+        const allIdOptions = document.querySelectorAll(".id");
+    } else if(selectedOption === "gender") {
+        const allGenderOptions = document.querySelectorAll(".gender");
+    } else if(selectedOption === "first name") {
+        const allFirstNameOptions = document.querySelectorAll(".first-name");
+    } else if(selectedOption === "last name") {
+        const allLastNameOptions = document.querySelectorAll(".last-name");
+    } else if(selectedOption === "birth date") {
+        const allBirthDateOptions = document.querySelectorAll(".birth-date");
+    } else if(selectedOption === "age") {
+        const allAgeOptions = document.querySelectorAll(".age");
+    } else if(selectedOption === "e-mail") {
+        const allEmailOptions = document.querySelectorAll(".e-mail");
+    } else if(selectedOption === "address") {
+        const allAddressOptions = document.querySelectorAll(".address");
+    } 
+})
+    
 
 const resetButton = document.querySelector(".reset-button");
 resetButton.addEventListener("click", () => {
     buttonResets();
+    sortAddingButton.disabled = false;
 })
 
 function buttonResets() {
@@ -124,12 +169,73 @@ sortDataButton.addEventListener("click", () => {
     sortDataButtonArea.setAttribute("data-sort-button-area-visible", "false");
     sortDataButton.setAttribute("data-sort-button-visible", "false");
     sortingArea.setAttribute("data-sort-fields-visible", "true");
+    let allBodyElements = document.body.querySelectorAll('*');
+    allBodyElements.forEach(function(element) {
+        if(element.classList.contains("not-blured") === false) {
+            element.classList.add("blured");
+        }
+    });
+  
 })
-
 const closeButton = document.querySelector(".close-button");
 closeButton.addEventListener("click", () => {
     sortDataButtonArea.setAttribute("data-sort-button-area-visible", "true");
     sortDataButton.setAttribute("data-sort-button-visible", "true");
     sortingArea.setAttribute("data-sort-fields-visible", "false");
     buttonResets();
+    let allBodyElements = document.body.querySelectorAll('*');
+    allBodyElements.forEach(element => element.classList.remove("blured"));
 })
+
+
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", () => {
+    const selectFields = document.querySelector(".sort-fields");
+    let selectedOption = selectFields.options[selectFields.selectedIndex].value;
+    if(selectedOption === "id") {
+        let idList = [];
+        data.forEach(element => idList.push(element.id));;
+        idList.sort();
+        for(let i = 0; i < idList.length; i++) {
+            for(let j = 0; j < data.length; j++) {
+                if(idList[i] === data[j].id) {
+                    idList[i] = data[j]
+    
+                }
+            } 
+        }
+        for(let i = 0; i < idList.length; i++) {
+            data[i] = idList[i];
+        }
+        console.log(data);
+        addAllDataAtOnce(data, createReferenceElement());
+    } else if(selectedOption === "gender") {
+        
+    } else if(selectedOption === "first name") {
+    
+    } else if(selectedOption === "last name") {
+    
+    } else if(selectedOption === "birth date") {
+    
+    } else if(selectedOption === "age") {
+    
+    } else if(selectedOption === "e-mail") {
+    
+    } else if(selectedOption === "address") {
+    
+    }
+    //console.log(selectedOption);
+})
+
+
+
+
+let idHeader = document.querySelector(".id-header");
+let genderHeader = document.querySelector(".gender-header");
+let firstNameHeader = document.querySelector(".first-name-header");
+let lastNameHeader = document.querySelector(".last-name-header");
+let birthDateHeader = document.querySelector(".birth-date-header");
+let ageHeader = document.querySelector(".age-header");
+let emailHeader = document.querySelector(".email-header");
+let addressHeader = document.querySelector(".address-header");
+
