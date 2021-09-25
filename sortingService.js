e70182211fbddb9b07beb4fe2d0f07214a74e91d
yurukyuruk <yurukyuruk@gmail.com>
2021-09-25 09:23:49 +0300

    sortingService improved

--- sortingService.js
+++ sortingService.js
@@ -17,76 +17,94 @@ import {MySortingSection} from "./sortModel.js";
         return this.sortConfig;
     }  
     sortData(data) {
-      if(this.sortConfig[0].type === "number" && this.sortConfig[0].direction === "ascending") {
-        data.sort(function(a, b) {
-          return a.age - b.age;
+      if(this.sortConfig[0].type === "number") {
+        data.sort((a, b) => {
+          if(this.sortConfig[0].direction === "ascending") {
+            return a.age - b.age;
+          } else if(this.sortConfig[0].direction === "descending") {
+            return b.age - a.age;
+          }  
         });
         return data; 
-      } else if(this.sortConfig[0].type === "number" && this.sortConfig[0].direction === "descending") {
-        data.sort(function(a, b) {
-          return b.age - a.age;
+      }
+
+      if(this.sortConfig[0].type === "date") {
+        data.sort((a, b) => {
+          let c = new Date(a.birthDate);
+          let d = new Date(b.birthDate);
+          if(this.sortConfig[0].direction === "ascending") {
+            return c - d;
+          } else if(this.sortConfig[0].direction === "descending") {
+            return d - c;
+          }
         });
         return data;
       }
 
-      if(this.sortConfig[0].type === "date" && this.sortConfig[0].direction === "ascending") {
-        data.sort(function(a, b) {
-          let c = new Date(a.birthDate);
-          let d = new Date(b.birthDate);
-          return c - d;
-        });
+      if(this.sortConfig[0].field === "id" && this.sortConfig[0].type === "string") {
+          data.sort((a,b)=> {
+            if(this.sortConfig[0].direction === "ascending") {
+              return a.id > b.id ? 1 : -1;
+            } else if(this.sortConfig[0].direction === "descending") {
+              return a.id < b.id ? 1 : -1;
+            } 
+          });
         return data;
-      } else if(this.sortConfig[0].type === "date" && this.sortConfig[0].direction === "descending") {
-        data.sort(function(a, b) {
-          let c = new Date(a.birthDate);
-          let d = new Date(b.birthDate);
-          return d - c;
+      } 
+
+      if(this.sortConfig[0].field === "gender" && this.sortConfig[0].type === "string") {
+        data.sort((a,b) => {
+          if(this.sortConfig[0].direction === "ascending") {
+            return a.gender > b.gender ? 1 : -1;
+          } else if(this.sortConfig[0].direction === "descending") {
+            a.gender < b.gender ? 1 : -1;
+          }
         })
         return data;
-      }
+      } 
 
-      if(this.sortConfig[0].field === "id" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "ascending") {
-        data.sort((a,b)=> (a.id > b.id ? 1 : -1))
+      if(this.sortConfig[0].field === "first name" && this.sortConfig[0].type === "string") {
+        data.sort((a,b) => {
+          if(this.sortConfig[0].direction === "ascending") {
+            return a.firstName > b.firstName ? 1 : -1;
+          } else if(this.sortConfig[0].direction === "descending") {
+            return a.firstName < b.firstName ? 1 : -1;
+          }
+        })
         return data;
-      } else if(this.sortConfig[0].field === "id" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "descending") {
-        data.sort((a,b)=> (a.id < b.id ? 1 : -1))
+      } 
+
+      if(this.sortConfig[0].field === "last name" && this.sortConfig[0].type === "string") {
+        data.sort((a,b) => {
+          if(this.sortConfig[0].direction === "ascending") {
+            return a.lastName > b.lastName ? 1 : -1;
+          } else if(this.sortConfig[0].direction === "descending") {
+            return a.lastName < b.lastName ? 1 : -1;
+          }
+        })
         return data;
-      }
-      if(this.sortConfig[0].field === "gender" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "ascending") {
-        data.sort((a,b)=> (a.gender > b.gender ? 1 : -1))
+      } 
+      
+      if(this.sortConfig[0].field === "e-mail" && this.sortConfig[0].type === "string") {
+        data.sort((a,b) => {
+          if(this.sortConfig[0].direction === "ascending") {
+            return a.email > b.email ? 1 : -1;
+          } else if(this.sortConfig[0].direction === "descending") {
+            return a.email < b.email ? 1 : -1;
+          }
+        })
         return data;
-      } else if(this.sortConfig[0].field === "gender" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "descending") {
-        data.sort((a,b)=> (a.gender < b.gender ? 1 : -1))
+      } 
+
+      if(this.sortConfig[0].field === "address" && this.sortConfig[0].type === "string") {
+        data.sort((a,b) => {
+          if(this.sortConfig[0].direction === "ascending") {
+            return a.address.city > b.address.city ? 1 : -1;
+          } else if(this.sortConfig[0].direction === "descending") {
+            return a.address.city < b.address.city ? 1 : -1;
+          }
+        })
         return data;
-      }
-      if(this.sortConfig[0].field === "firstName" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "ascending") {
-        data.sort((a,b)=> (a.firstName > b.firstName ? 1 : -1))
-        return data;
-      } else if(this.sortConfig[0].field === "firstName" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "descending") {
-        data.sort((a,b)=> (a.firstName < b.firstName ? 1 : -1))
-        return data;
-      }
-      if(this.sortConfig[0].field === "lastName" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "ascending") {
-        data.sort((a,b)=> (a.lastName > b.lastName ? 1 : -1))
-        return data;
-      } else if(this.sortConfig[0].field === "lastName" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "descending") {
-        data.sort((a,b)=> (a.lastName < b.lastName ? 1 : -1))
-        return data;
-      }
-      if(this.sortConfig[0].field === "e-mail" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "ascending") {
-        data.sort((a,b)=> (a.email > b.email ? 1 : -1))
-        return data;
-      } else if(this.sortConfig[0].field === "e-mail" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "descending") {
-        data.sort((a,b)=> (a.email < b.email ? 1 : -1))
-        return data;
-      }
-      if(this.sortConfig[0].field === "address" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "ascending") {
-        data.sort((a,b)=> (a.address > b.address ? 1 : -1))
-        return data;
-      } else if(this.sortConfig[0].field === "address" && this.sortConfig[0].type === "string" && this.sortConfig[0].direction === "descending") {
-        data.sort((a,b)=> (a.address < b.address ? 1 : -1))
-        return data;
-      }
-    
+      } 
     }  
   }
